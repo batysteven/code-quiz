36adb4ffc5990437e61715561b03b12306e672d8
Steven Baty <baty_steven@yahoo.com>
2020-11-23 00:48:30 -0700

    loop through all questions

--- assets/script.js
+++ assets/script.js
@@ -82,19 +82,21 @@ function showQuestion(question) {
 };
 
 function resetState() {
+    clearStatusClass(document.body);
     nextButton.classList.add('hide');
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
 }
 
-function selectAnswer() {
+function selectAnswer(e) {
     const selectedButton = e.target;
     const correct = selectedButton.dataset.correct;
     setStatusClass(document.body, correct);
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
+    nextButton.classList.remove('hide');
 };
 
 function setStatusClass(element, correct) {
@@ -106,4 +108,14 @@ function setStatusClass(element, correct) {
     }
 }
 
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
+
+}
+
 startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+    answeredQuestionIndex++
+    setNextQuestion();
+});
